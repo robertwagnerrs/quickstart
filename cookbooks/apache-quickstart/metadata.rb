512e531c3d61fb4149fd1667f80013cd0ae407b2
Robert Wagner <robert.wagner@rightscale.com>
2011-11-03 20:59:09 +0000

    more changes

--- cookbooks/apache-quickstart/metadata.rb
+++ cookbooks/apache-quickstart/metadata.rb
@@ -6,3 +6,9 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
 recipe "apache-quickstart::default", "Install Apache"
+
+attribute "quickstart/name",
+:display_name => "Robert",
+:description => "Robert",
+:required => true,
+:recipes => [ "apache-quickstart::default" ]
